229f8f8010bec99f142d4c94420176fb97a1984d
Nicolas De Maio <nicolas.demaio19@gmail.com>
2021-03-27 15:03:56 -0300

    Initial commit

---
+++ src/main/java/com/ndemaio/studentprojectspring/StudentProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ndemaio.studentprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentProjectSpringApplication.class, args);
+	}
+
+}
